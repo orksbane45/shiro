aba6111990f43f165bbb0b4d4286cb296f8861ae
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2019-01-28 20:16:05 +0100

    Update setwaifu.js

--- commands/setwaifu.js
+++ commands/setwaifu.js
@@ -9,7 +9,7 @@ exports.run = function(client, message, args) {
   
     let embed1 = new Discord.RichEmbed()
     .setTitle(`Character`)
-    .setDescription(`A waifu/husbando appeared!\rTry guessing their name with w.claim <name> to claim them!\r\rHints:\rThis character's initials are 'J.'\rUse \`w.lookup <name>\` if you can't remember the full name.\r\r(If the image is missing, click here.)`)
+    .setDescription(`A waifu/husbando appeared!\rTry guessing their name with \`w.claim <name>\` to claim them!\r\rHints:\rThis character's initials are 'J.'\rUse \`w.lookup <name>\` if you can't remember the full name.\r\r(If the image is missing, click here.)`)
     .setColor("#ff5000")
     .setImage("https://cdn.myanimelist.net/images/characters/12/248121.jpg")
     message.channel.send(embed1);
